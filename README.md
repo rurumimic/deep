316cddbc20126600ea072c0abc12d8d529c9ce12
rurumimic <unhyop@gmail.com>
2020-08-15 12:07:59 +0900

    퍼셉트론 함수: AND, NAND, OR, XOR

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # deep
+
 딥러닝
+
+## 환경 설정
+
+### Python 3.8
+
+```bash
+conda create -n deep python=3.8
+conda activate deep
+```
+
+### Libraries
+
+```bash
+conda install -c anaconda numpy
+```
+
+### Jupyter Lab
+
+```bash
+conda install -c conda-forge jupyterlab
+jupyter lab
+```
---
+++ perceptron/perceptron.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+def func(x, w, b):
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    return func(x, w, b)
+    
+    
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    return func(x, w, b)
+    
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    return func(x, w, b)
+    
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
